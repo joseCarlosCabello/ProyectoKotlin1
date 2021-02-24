0f7681e28105abd63ebb047262a2f401a68c9ee1
joseCarlosCabello <tsunayoshi598@gmail.com>
2021-02-24 13:02:56 -0600

    Operadores

--- src/main/kotlin/HolaMundo.kt
+++ src/main/kotlin/HolaMundo.kt
@@ -1,4 +1,17 @@
 fun main(args: Array<String>)
 {
     println("adios mundo")
+    var x=10
+    var y=20
+    var z=70
+
+    println("Suma:  "+  x.plus(y).plus(z))
+
+    println("Resta:  "+ x.minus(y).minus(z))
+
+    println("Division:  "+ x.div(y).div(z))
+
+    println("Multiplicacion:  "+ x.times(y).times(z))
+
+
 }
